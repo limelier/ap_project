cc328a3db9732ec7b99521a6d79c5f3a1f47c0d2
Iacobescu Tudor <tudor.iacobescu.1@gmail.com>
2020-05-29 22:26:15 +0300

    Replaced piece icons with custom ones.

--- front/src/main/kotlin/App.kt
+++ front/src/main/kotlin/App.kt
@@ -11,15 +11,27 @@ class App : Application() {
         val root = TilePane()
         root.alignment = Pos.CENTER
 
-        val tile = Tile(TileColor.WHITE, Piece(Color.BLACK, Rank.QUEEN))
+        var tile = Tile(TileColor.WHITE)
 
-        root.children.add(tile)
+        for (i in 0..8) {
+            for (j in 0..8) {
+                tile = Tile(
+                    when((i + j) % 2 == 0) {
+                        true -> TileColor.BLACK
+                        false -> TileColor.WHITE
+                    },
+                    null
+                )
+                root.children.add(tile)
+            }
+        }
+
+        tile.displayPiece(Piece(Color.WHITE, Rank.QUEEN))
 
         val scene = Scene(root, 800.0, 600.0)
         primaryStage!!.scene = scene
         primaryStage.show()
 
-        tile.displayPiece(Piece(Color.WHITE, Rank.PAWN))
     }
 }
 
