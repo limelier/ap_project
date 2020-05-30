aab9f1b1d74a6702a0771129a9320d846a7ee528
Iacobescu Tudor <tudor.iacobescu.1@gmail.com>
2020-05-30 09:51:37 +0300

    Piece images now loaded into memory only once.

--- front/src/main/kotlin/Piece.kt
+++ front/src/main/kotlin/Piece.kt
@@ -1,3 +1,6 @@
+import javafx.scene.image.Image
+import java.io.FileInputStream
+
 enum class Color {
     WHITE,
     BLACK
@@ -15,4 +18,32 @@ enum class Rank {
 data class Piece(
     val color: Color,
     val rank: Rank
-)
+) {
+    val image: Image
+    get() = getImage(this)
+
+    companion object {
+        private val pieceImages: Map<Piece, Image>
+
+        private fun loadImage(piece: Piece): Image {
+            val color = piece.color.name.toLowerCase()
+            val rank = piece.rank.name.toLowerCase()
+
+            return Image(FileInputStream("src/main/resources/images/${color}_${rank}.png"))
+        }
+
+        @Suppress("MapGetWithNotNullAssertionOperator")
+        // the map is populated on init, NPE cannot occur
+        private fun getImage(piece: Piece): Image {
+            return pieceImages[piece]!!
+        }
+
+        init {
+            val ranks = Rank.values()
+            val colors = Color.values()
+            val pieces = ranks.flatMap { rank -> colors.map { color -> Piece(color, rank) } }
+            pieceImages = pieces.associateWith { loadImage(it) }
+        }
+    }
+}
+
--- front/src/main/kotlin/Tile.kt
+++ front/src/main/kotlin/Tile.kt
@@ -1,8 +1,6 @@
 import javafx.geometry.Pos
-import javafx.scene.image.Image
 import javafx.scene.image.ImageView
 import javafx.scene.layout.StackPane
-import java.io.FileInputStream
 
 enum class TileColor {
     WHITE,
@@ -10,32 +8,12 @@ enum class TileColor {
 }
 
 class Tile(color: TileColor, piece: Piece? = null) : StackPane() {
-    companion object {
-        val pieceImages: Map<Piece, Image>
 
-        private fun imageForPiece(piece: Piece): Image {
-            val color = piece.color.name.toLowerCase()
-            val rank = piece.rank.name.toLowerCase()
-
-            return Image(FileInputStream("src/main/resources/images/${color}_${rank}.png"))
-        }
-
-        init {
-            val ranks = Rank.values()
-            val colors = Color.values()
-            val pieces = ranks.flatMap { rank -> colors.map { color -> Piece(color, rank) } }
-            pieceImages = pieces.associateWith { imageForPiece(it) }
-        }
-    }
 
     private val imageView = ImageView()
 
     fun displayPiece(piece: Piece?) {
-        imageView.image = if (piece == null) {
-            null
-        } else {
-            pieceImages[piece]
-        }
+        imageView.image = piece?.image
     }
 
     init {
