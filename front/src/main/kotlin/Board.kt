540bbe8eb210d7aa46b8d7e753a352c3939a0124
Iacobescu Tudor <tudor.iacobescu.1@gmail.com>
2020-05-30 10:31:01 +0300

    Separated board state into serializable Board class.

---
+++ front/src/main/kotlin/Board.kt
@@ -0,0 +1,25 @@
+data class Board(
+    var name: String = "",
+    var description: String = "",
+    var pieces: Array<Array<Piece?>> = Array(8) { arrayOfNulls(8) }
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Board
+
+        if (name != other.name) return false
+        if (description != other.description) return false
+        if (!pieces.contentDeepEquals(other.pieces)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + description.hashCode()
+        result = 31 * result + pieces.contentDeepHashCode()
+        return result
+    }
+}
--- front/src/main/kotlin/State.kt
+++ front/src/main/kotlin/State.kt
@@ -1,9 +1,5 @@
 class State {
     var selectedColor = Color.WHITE
     var selectedRank: Rank? = null
-
-    var name: String = ""
-    var description: String = ""
-
-    var pieces: Array<Array<Piece?>> = Array(8) { arrayOfNulls(8) }
+    private var board: Board = Board()
 }
