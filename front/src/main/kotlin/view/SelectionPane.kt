8a9de16d7facee620a485b2a5cb733a14cb80f71
Iacobescu Tudor <tudor.iacobescu.1@gmail.com>
2020-05-31 14:32:33 +0300

    Clean up SelectionPane using Kotlin idioms
    Using the extension function .apply, you can easily do a lot of things to a variable without having to name it every time. This can even be used for initialization.

--- front/src/main/kotlin/view/SelectionPane.kt
+++ front/src/main/kotlin/view/SelectionPane.kt
@@ -18,60 +18,61 @@ class SelectionPane : VBox() {
 
     private val pieceButtons = Rank.values().map { PieceButton(Piece(Color.WHITE, it)) }
 
-    private fun changePieceButtonColors(color: Color) {
-        pieceButtons.forEach { it.changeColor(color) }
-    }
+    private fun changePieceButtonColors(color: Color) = pieceButtons.forEach { it.changeColor(color) }
 
     init {
-        val colorRoot = initColorRoot()
-        val pieceRoot = initPieceRoot()
-        children.addAll(colorRoot, Separator(Orientation.HORIZONTAL), pieceRoot)
+        children.addAll(initColorRoot(), Separator(Orientation.HORIZONTAL), initPieceRoot())
         alignment = Pos.CENTER
         spacing = 5.0
         padding = Insets(0.0, 20.0, 0.0, 20.0)
     }
 
     private fun initPieceRoot(): VBox {
-        val pieceRoot = VBox()
-        val btnDelete = SelectButton("Erase")
-        btnDelete.isSelected = true
-        btnDelete.prefWidthProperty().bind(pieceButtons[0].widthProperty())
-        btnDelete.prefHeight = 64.0
-        val rankGroup = ToggleGroup()
-        rankGroup.toggles.add(btnDelete)
-        rankGroup.toggles.addAll(pieceButtons)
-        rankGroup.selectedToggleProperty().addListener { _, _, value ->
-            val rank = when (value) {
-                btnDelete -> null
-                is PieceButton -> value.getRank()
-                else -> throw Error("Invalid rank toggle.")
-            }
-            rankProperty.set(rank)
+        val btnDelete = SelectButton("Erase").apply {
+            isSelected = true
+            prefWidthProperty().bind(pieceButtons[0].widthProperty())
+            prefHeight = 64.0
         }
-        pieceRoot.children.add(btnDelete)
-        pieceRoot.children.addAll(pieceButtons)
-        pieceRoot.alignment = Pos.CENTER
-        return pieceRoot
+        ToggleGroup().apply {
+            toggles.add(btnDelete)
+            toggles.addAll(pieceButtons)
+            selectedToggleProperty().addListener { _, _, value ->
+                val rank = when (value) {
+                    btnDelete -> null
+                    is PieceButton -> value.getRank()
+                    else -> throw Error("Invalid rank toggle.")
+                }
+                rankProperty.set(rank)
+            }
+        }
+        return VBox()
+            .apply {
+            children.add(btnDelete)
+            children.addAll(pieceButtons)
+            alignment = Pos.CENTER
+        }
     }
 
     private fun initColorRoot(): HBox {
-        val colorRoot = HBox()
         val btnWhite = SelectButton("White")
         btnWhite.isSelected = true
         val btnBlack = SelectButton("Black")
-        val colorGroup = ToggleGroup()
-        colorGroup.toggles.addAll(btnWhite, btnBlack)
-        colorGroup.selectedToggleProperty().addListener { _, _, value ->
-            val color = when (value) {
-                btnWhite -> Color.WHITE
-                btnBlack -> Color.BLACK
-                else -> throw Error("Invalid color toggle.")
+        ToggleGroup().apply {
+            toggles.addAll(btnWhite, btnBlack)
+            selectedToggleProperty().addListener { _, _, value ->
+                val color = when (value) {
+                    btnWhite -> Color.WHITE
+                    btnBlack -> Color.BLACK
+                    else -> throw Error("Invalid color toggle.")
+                }
+                colorProperty.set(color)
+                changePieceButtonColors(color)
             }
-            colorProperty.set(color)
-            changePieceButtonColors(color)
         }
-        colorRoot.children.addAll(btnWhite, btnBlack)
-        colorRoot.alignment = Pos.CENTER
-        return colorRoot
+
+        return HBox().apply {
+            children.addAll(btnWhite, btnBlack)
+            alignment = Pos.CENTER
+        }
     }
 }
